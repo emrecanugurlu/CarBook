1ffeed907e2ccda87889230071200a1109e3cafe
Emre Can <emrecnugurlu@gmail.com>
2025-03-02 02:01:57 +0300

    Add project files.

---
+++ Core/CarBook.Domain/Entities/CarFeature.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities
+{
+    public class CarFeature
+    {
+        public int CarFeatureId { get; set; }
+        public bool Aveilable { get; set; }
+        public Car Car { get; set; }
+        public Feature Feature { get; set; }
+    }
+}
